370ac5cb20213da4fced0e2dbc27816b366e6579
Abdul Azeem <abdulazeem@devnexus.in>
2025-09-29 13:10:41 +0530

    change file name

--- config/connectDashboardDB.js
+++ config/connectDashboardDB.js
@@ -1,23 +1,26 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 let dashboardDB = null;
 
 export const getDashboardDB = async () => {
-    if (!dashboardDB) {
-        try {
-            console.log("DB URI : ", process.env.DASHBOARD_DB_URI);
-            
-            dashboardDB = await mongoose.createConnection(process.env.DASHBOARD_DB_URI, {
-                // useNewUrlParser: true,
-                // useUnifiedTopology: true,
-            });
-            console.log('Connected to Dashboard DB');
-        } catch (error) {
-            console.error('Dashboard DB connection error:', error.message);
+  if (!dashboardDB) {
+    try {
+      console.log("DB URI : ", process.env.DASHBOARD_DB_URI);
+
+      dashboardDB = await mongoose.createConnection(
+        process.env.DASHBOARD_DB_URI,
+        {
+          // useNewUrlParser: true,
+          // useUnifiedTopology: true,
         }
+      );
+      console.log("Connected to Dashboard DB");
+    } catch (error) {
+      console.error("Dashboard DB connection error:", error.message);
     }
-    return dashboardDB;
+  }
+  return dashboardDB;
 };
