d43fa0cffba9d08523cedd92401c81c8a58c9d4a
architdevnexus <ankit@devnexus.in>
2026-05-26 14:06:01 +0530

    newletter-subscribe

--- controllers/newsletterControllers.js
+++ controllers/newsletterControllers.js
@@ -2,52 +2,64 @@ import nodemailer from "nodemailer";
 import NewsletterModel from "../model/newsLetterModel.js";
 
 export const subscribeToNewsletter = async (req, res) => {
-  const { email } = req.query;
+  const { email } = req.body;
 
   if (!email) {
-    return res
-      .status(400)
-      .json({ success: false, message: "Email is required" });
+    return res.status(400).json({
+      success: false,
+      message: "Email is required",
+    });
   }
 
   try {
-    const newsLetter = await NewsletterModel.findOne({ email });
-    if (newsLetter) {
-      return res
-        .status(400)
-        .json({ success: false, message: "Email already subscribe" });
+    // Check existing email
+    const existingEmail = await NewsletterModel.findOne({ email });
+
+    if (existingEmail) {
+      return res.status(400).json({
+        success: false,
+        message: "Email already subscribed",
+      });
     }
 
+    // Save email
     const newSubscription = new NewsletterModel({ email });
+
     await newSubscription.save();
 
+    // SMTP transporter
     const transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST,
       port: Number(process.env.SMTP_PORT),
-      secure: Number(process.env.SMTP_PORT),
+      secure: Number(process.env.SMTP_PORT) === 465,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
     });
 
-    const mailOptions = {
-      from: `"Your Brand" <${process.env.EMAIL_USER}>`,
+    // Send mail
+    await transporter.sendMail({
+      from: `"DevNexus Solutions" <${process.env.EMAIL_USER}>`,
       to: email,
-      subject: "Welcome to our newsletter",
-      html: `<h2>Welcome to Our Newsletter!</h2>
-            <p>We're glad to have you onboar. You'll receive regular update from us.</p>`,
-    };
+      subject: "Welcome to DevNexus Newsletter",
+      html: `
+        <h2>Welcome to Our Newsletter 🎉</h2>
+        <p>Thank you for subscribing to DevNexus Solutions.</p>
+        <p>You will now receive updates, news, and offers from us.</p>
+      `,
+    });
 
-    await transporter.sendMail(mailOptions);
-    res.status(200).json({
+    return res.status(200).json({
       success: true,
-      message: "Subscribed and confirmation email sent",
+      message: "Subscribed successfully!",
     });
   } catch (error) {
-    console.error("Subscription error", error);
-    res
-      .status(500)
-      .json({ success: false, message: "failed to subscribe", error });
+    console.log("NEWSLETTER ERROR =>", error);
+
+    return res.status(500).json({
+      success: false,
+      message: error.message || "Failed to subscribe",
+    });
   }
 };
